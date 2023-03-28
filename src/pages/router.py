ebdd55f5b5bb3336806668e7410ea397bca37da7
Roman Stryuk <rstryuk@gmail.com>
2023-03-28 14:36:19 +0200

    listng and lot view

--- src/pages/router.py
+++ src/pages/router.py
@@ -27,14 +27,15 @@ async def get_base_page(request: Request):
         r = await client.get(url)
         lots = r.json()
         # print(lots)
-        return templates.TemplateResponse("listing.html", {'request': request, 'lots': lots})
+        return templates.TemplateResponse("listing.html", {'request': request, 'lots': lots, 'backend': BACKEND_URL})
     except Exception as e:
         print(e)
         lots = []
+
         return templates.TemplateResponse("listing.html", {'request': request})
 
 
-@router.get('/lot/{lot_id}')
+@ router.get('/lot/{lot_id}')
 async def display_lot(request: Request, lot_id):
     url: URL = f'{BACKEND_URL}/api/v1/lot/{lot_id}'
 
@@ -43,14 +44,14 @@ async def display_lot(request: Request, lot_id):
     try:
         r = await client.get(url)
         lot = r.json()
-        # print(lots)
-        return templates.TemplateResponse("lot.html", {'request': request, 'lot': lot})
+        print(lot)
+        return templates.TemplateResponse("lot.html", {'request': request, 'lot': lot, 'backend': BACKEND_URL})
     except Exception as e:
         print(e)
-        lots = {}
+        lot = {}
         return templates.TemplateResponse("lot.html", {'request': request})
 
 
-@router.get('/login')
+@ router.get('/login')
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {'request': request})
